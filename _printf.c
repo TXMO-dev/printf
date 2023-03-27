031dd02294d2f683b34c0f2c9bdb0e7a20db2303
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-03-27 09:49:46 -0700

    this is it

--- _printf.c
+++ _printf.c
@@ -1,84 +1,71 @@
 #include <stdarg.h>
-#include <stdio.h>
 #include "main.h"
 /**
-* _putchar - writes a character to stdout
-* @c: The character to print
+* _puts - prints a string to stdout
+* @str: string to be printed
 *
-* Return: 1 on success, -1 on error
+* Return: number of characters printed
 */
-int _putchar(char c)
+int _puts(char *str)
 {
-return (write(1, &c, 1));
+int len = 0;
+char c;
+while ((c = *(str++)))
+{
+_putchar(c);
+len++;
+}
+return (len);
 }
 /**
-* _printf - produces output according to a format
-* @format: character string containing directives
-*
-* Return: the number of characters printed (excluding the null byte)
-*/
+ * _printf - prints a formatted string to stdout
+ * @format: format string containing zero or more directives
+ *
+ * Return: number of characters printed
+ */
 int _printf(const char *format, ...)
 {
-va_list args;
-int count = 0;
-va_start(args, format);
-while (*format)
-{
-if (*format == '%')
-{
-format++;
-if (*format == 'c')
-{
-count += _putchar(va_arg(args, int));
-}
-else if (*format == 's')
-{
-count += printf("%s", va_arg(args, char *));
-}
-else if (*format == 'd' || *format == 'i')
-{
-count += printf("%d", va_arg(args, int));
-}
-else if (*format == 'b')
-{
-unsigned int num = va_arg(args, unsigned int);
-int bit = 0;
-char binary[32];
-if (num == 0)
-{
-count += _putchar('0');
-}
-else
-{
-while (num > 0)
-{
-binary[bit++] = (num & 1) ? '1' : '0';
-num >>= 1;
-}
-while (bit--)
-{
-count += _putchar(binary[bit]);
-}
-}
-}
-else if (*format == '%')
-{
-count += _putchar('%');
-}
-else
-{
-_putchar('%');
-_putchar(*format);
-count += 2;
+    va_list args;
+    int len = 0;
+
+    va_start(args, format);
+
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+
+            switch (*format)
+            {
+                case 'c':
+                    _putchar(va_arg(args, int));
+                    len++;
+                    break;
+
+                case 's':
+                    len += _puts(va_arg(args, char *));
+                    break;
+
+                case '%':
+                    _putchar('%');
+                    len++;
+                    break;
+
+                default:
+                    _putchar('%');
+                    _putchar(*format);
+                    len += 2;
+		    break;
 }
 }
 else
 {
 _putchar(*format);
-count++;
+len++;
 }
 format++;
 }
 va_end(args);
-return (count);
+return (len);
 }
