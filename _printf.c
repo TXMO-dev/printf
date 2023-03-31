d76008334a722ac474daba192c1893749d17251b
Ebrahim Ghaith <ebrahimghaith7@gmail.com>
2023-03-31 14:06:45 +0200

    fourth try

--- _printf.c
+++ _printf.c
@@ -1,6 +1,45 @@
 #include "main.h"
 #include <unistd.h>
 /**
+ * get_function - function specifiers.
+ *
+ * @con_spec: change format
+ * @args: aguments
+ *
+ * Return: counter.
+ */
+
+int get_function(char con_spec, va_list args)
+{
+	int i = 0;
+	int count_fun = 0;
+
+	specifiers_t spec[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_mod},
+		{'d', print_digit},
+		{'i', print_digit},
+		{'r', print_rev_string},
+		{0, NULL}
+	};
+
+	while (spec[i].specifiers)
+	{
+		if (con_spec == spec[i].specifiers)
+			count_fun += spec[i].f(args);
+		i++;
+	}
+
+	if (count_fun == 0)
+	{
+		count_fun += _putchar('%');
+		count_fun += _putchar(con_spec);
+	}
+
+	return (count_fun);
+}
+/**
  * _printf - like printf.
  *
  * @format: Format of(c,s,d).
